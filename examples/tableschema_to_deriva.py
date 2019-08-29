10dc0bb3d10a5f24037f0eb486cba1abbe36b049
Karl Czajkowski <karlcz@isi.edu>
2019-08-28 17:39:18 -0700

    simple demo script to translate table-schema JSON to deriva equivalent
    This handles the subset of features being used in
    examples/cfde-core-mode.json

---
+++ examples/tableschema_to_deriva.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+
+"""Translate basic Frictionless Table-Schema table definitions to Deriva.
+
+- Reads table-schema JSON on standard input
+- Writes deriva schema JSON on standard output
+
+The output JSON is suitable for POST to an /ermrest/catalog/N/schema
+resource on a fresh, empty catalog.
+
+Example:
+
+   cd cfde-deriva
+   python3 examples/tableschema_to_deriva.py \
+     < table-schema/cfde-core-model.json
+
+"""
+
+import sys
+import json
+from deriva.core.ermrest_model import builtin_types, Table, Column, Key, ForeignKey
+
+tableschema = json.load(sys.stdin)
+resources = tableschema['resources']
+
+# translate table-schema definitions into deriva definitions
+
+schema_name = 'CFDE'
+
+def make_type(type, format):
+    """Choose appropriate ERMrest column types..."""
+    if type == "string":
+        return builtin_types.text
+    if type == "datetime":
+        return builtin_types.timestamptz
+    if type == "integer":
+        return builtin_types.int8
+    if type == "number":
+        return builtin_types.float8
+    raise ValueError('no mapping defined yet for type=%s format=%s' % (type, format))
+
+def make_column(cdef):
+    constraints = cdef.get("constraints", {})
+    return Column.define(
+        cdef["name"],
+        make_type(
+            cdef.get("type", "string"),
+            cdef.get("format", "default"),
+        ),
+        nullok=not constraints.get("unique", False),
+        comment=cdef.get("description"),
+    )
+
+def make_key(tname, cols):
+    return Key.define(
+        cols,
+        constraint_names=[ [schema_name, "%s_%s_key" % (tname, "_".join(cols))] ],
+    )
+
+def make_fkey(tname, fkdef):
+    fkcols = fkdef["fields"]
+    fkcols = [fkcols] if isinstance(fkcols, str) else fkcols
+    reference = fkdef["reference"]
+    pktable = reference["resource"]
+    pktable = tname if pktable == "" else pktable
+    pkcols = reference["fields"]
+    pkcols = [pkcols] if isinstance(pkcols, str) else pkcols
+    return ForeignKey.define(
+        fkcols,
+        schema_name,
+        pktable,
+        pkcols,
+        constraint_names=[ [schema_name, "%s_%s_fkey" % (tname, "_".join(fkcols))] ],
+    )
+
+def make_table(tdef):
+    tname = tdef["name"]
+    keys = []
+    pk = tdef.get("primaryKey")
+    if isinstance(pk, str):
+        pk = [pk]
+    if isinstance(pk, list):
+        keys.append(make_key(tname, pk))
+    return Table.define(
+        tname,
+        column_defs=[
+            make_column(cdef)
+            for cdef in tdef["fields"]
+        ],
+        key_defs=([ make_key(tname, pk) ] if pk else []) + [
+            make_key(tname, [cdef["name"]])
+            for cdef in tdef["fields"]
+            if cdef.get("constraints", {}).get("unique", False)
+        ],
+        fkey_defs=[
+            make_fkey(tname, fkdef)
+            for fkdef in tdef.get("foreignKeys", [])
+        ],
+        comment=tdef.get("description"),
+    )
+
+deriva_schema = {
+    "schemas": {
+        schema_name: {
+            "schema_name": schema_name,
+            "tables": {
+                tdef["name"]: make_table(tdef)
+                for tdef in resources
+            }
+        }
+    }
+}
+
+json.dump(deriva_schema, sys.stdout, indent=2)
