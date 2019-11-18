f20bbe6c5193fe172625ae499a2d5b351fe5cbb8
Karl Czajkowski <karlcz@isi.edu>
2019-11-18 14:18:02 -0800

    fixup package version info

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 setup(
     name='cfde_deriva',
     description='CFDE Deriva Integration Utilities',
-    version='0.2',
+    version='0.3',
     packages=[
         'cfde_deriva',
     ],
     package_data={},
     scripts=[],
     requires=['deriva'],
-    install_requires=['deriva'],
+    install_requires=['deriva>=1.0'],
     maintainer_email='support@misd.isi.edu',
     classifiers=[
         'Intended Audience :: Developers',
