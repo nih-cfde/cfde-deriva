02fd38eeb7f2cbec6dad9c9ddb90122b90df2fac
Karl Czajkowski <karlcz@isi.edu>
2020-10-12 12:51:12 -0700

    add script documenting manual build procedure
    This script just extracts the list of SQL files in document order
    from the custom datapackage.  A more appropriate automation method
    might interpret the datapackag to locate these SQL defintions,
    but execute them in a different ETL processing environment...

---
+++ table-schema/generate_demo_etl_sql.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cat <<EOF
+BEGIN;
+
+SET search_path = "CFDE";
+
+-- delete in case we have previous ETL results?
+DELETE FROM collection_anatomy;
+DELETE FROM collection_assay_type;
+DELETE FROM collection_biosample_creation_time;
+DELETE FROM collection_in_collection_transitive;
+DELETE FROM collection_subject_granularity;
+DELETE FROM collection_subject_role_taxonomy;
+DELETE FROM file_anatomy;
+DELETE FROM file_biosample_creation_time;
+DELETE FROM file_subject_granularity;
+DELETE FROM file_subject_role_taxonomy;
+DELETE FROM project_in_project_transitive;
+DELETE FROM project_root;
+
+EOF
+
+grep derivation c2m2-level1-portal-model.json \
+    | sed -e 's/.*: "\([^"]*\)"/\1/' \
+    | while read sql_fname
+do
+    cat <<EOF
+
+-- begin ${sql_fname}
+EOF
+    cat ${sql_fname}
+    cat <<EOF
+-- end ${sql_fname}
+
+EOF
+   
+done
+
+cat <<EOF
+COMMIT;
+EOF
