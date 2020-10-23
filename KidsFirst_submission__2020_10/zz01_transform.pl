2c70b0531e3cf999b29ee9c49f77ec6ff2d8ddb4
abradyIGS <achron@gmail.com>
2020-10-23 15:20:14 -0400

    Updating KF submission

--- KidsFirst_submission__2020_10/zz01_transform.pl
+++ KidsFirst_submission__2020_10/zz01_transform.pl
@@ -27,11 +27,49 @@ foreach my $staticStub ( 'assay_type.tsv', 'biosample_in_collection.tsv', 'biosa
    system("cp $stubDir/$staticStub $outDir");
 }
 
+my $assayType = {};
+
+open IN, "<000_original/biosample.tsv" or die("Can't open 000_original/biosample.tsv for reading.\n");
+
+while ( chomp( my $line = <IN> ) ) {
+   
+   my @fields = split(/\t/, $line);
+
+   my $id = $fields[1];
+
+   my $at_val = $fields[6];
+
+   if ( $at_val ne '' ) {
+      
+      $assayType->{$id} = $at_val;
+   }
+}
+
+close IN;
+
+open IN, "<001_static_stubs.some_newly_created_with_abhijna/file_describes_biosample.tsv" or die("Can't open 001_static_stubs.some_newly_created_with_abhijna/file_describes_biosample.tsv for reading.\n");
+
+my $header = <IN>;
+
+my $fileAssayType = {};
+
+while ( chomp( my $line = <IN> ) ) {
+   
+   my ( $fileNS, $fileID, $sampleNS, $sampleID ) = split(/\t/, $line);
+
+   if ( exists( $assayType->{$sampleID} ) ) {
+      
+      $fileAssayType->{$fileID} = $assayType->{$sampleID};
+   }
+}
+
+close IN;
+
 open IN, "<$inDir/file.tsv" or die("Can't open $inDir/file.tsv for reading.\n");
 
 open OUT, ">$outDir/file.tsv" or die("Can't open $outDir/file.tsv for writing.\n");
 
-my $header = <IN>;
+$header = <IN>;
 
 print OUT join("\t",
                      'id_namespace',
@@ -54,9 +92,16 @@ while ( chomp( my $line = <IN> ) ) {
    
    my ( $id_ns, $local_id, $project_id_ns, $project_local_id, $persistent_id, $creation_time, $size_in_bytes, $sha256, $md5, $filename, $file_format, $data_type ) = split(/\t/, $line);
 
+   my $at_val = '';
+
+   if ( exists( $fileAssayType->{$local_id} ) ) {
+      
+      $at_val = $fileAssayType->{$local_id};
+   }
+
    print OUT "$id_ns\t$local_id\t$project_id_ns\t$project_local_id\t$persistent_id\t$creation_time\t";
 
-   print OUT "$size_in_bytes\t\t$sha256\t$md5\t$filename\t$file_format\t$data_type\t\tapplication/octet-stream\n";
+   print OUT "$size_in_bytes\t\t$sha256\t$md5\t$filename\t$file_format\t$data_type\t$at_val\tapplication/octet-stream\n";
 }
 
 close OUT;
