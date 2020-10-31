34e01453c77f2eb0192ba38ba5fb4c4bf01eb2d4
Karl Czajkowski <karlcz@isi.edu>
2020-10-30 17:37:42 -0700

    revise level1 file stats to reduce multi-counting
    get distinct entity-term combos to remove duplicates as much as
    possible before reducing to sub-totals.
    
    this gets byte counts to be closer to the file counts, only
    double-counting because of files actually appearing in more than one
    group's sub-total.

--- table-schema/level1_stats.sql
+++ table-schema/level1_stats.sql
@@ -76,51 +76,66 @@ SELECT
 FROM ( (
   -- file stats need file table rows, find optional remote metadata
   SELECT
-    pr.project_id_namespace AS root_project_id_namespace,
-    pr.project_local_id AS root_project_local_id,
-    f.project_id_namespace AS project_id_namespace,
-    f.project_local_id AS project_local_id,
-    COALESCE(f.assay_type, '') AS assay_type_id,
-    COALESCE(f.data_type, '') AS data_type_id,
-    COALESCE(f.file_format, '') AS file_format_id,
-    COALESCE(b.anatomy, '') AS anatomy_id,
-    COALESCE(s.granularity, '') AS granularity_id,
-    COALESCE(ss.species, '') AS species_id,
-    count(DISTINCT f."RID") AS num_files,
-    sum(f.size_in_bytes) AS num_bytes
-  FROM file f
-  JOIN project_in_project_transitive pipt
-    ON (    f.project_id_namespace = pipt.member_project_id_namespace
-        AND f.project_locaL_id = pipt.member_project_local_id)
-  JOIN project_root pr
-    ON (    pipt.leader_project_id_namespace = pr.project_id_namespace
-        AND pipt.leader_project_local_id = pr.project_local_id)
-  LEFT JOIN file_describes_biosample fdb
-    ON (    f.id_namespace = fdb.file_id_namespace
-        AND f.local_id = fdb.file_local_id)
-  LEFT JOIN biosample b
-    ON (    fdb.biosample_id_namespace = b.id_namespace
-        AND fdb.biosample_local_id = b.local_id)
-  LEFT JOIN biosample_from_subject bfs
-    ON (    bfs.biosample_id_namespace = b.id_namespace
-        AND bfs.biosample_local_id = b.local_id)
-  LEFT JOIN subject s
-    ON (    bfs.subject_id_namespace = s.id_namespace
-        AND bfs.subject_local_id = s.local_id)
-  LEFT JOIN subject_species ss
-    ON (    s.id_namespace = ss.subject_id_namespace
-        AND s.local_id = ss.subject_local_id)
+    root_project_id_namespace,
+    root_project_local_id,
+    project_id_namespace,
+    project_local_id,
+    COALESCE(assay_type_id, '') AS assay_type_id,
+    COALESCE(data_type_id, '') AS data_type_id,
+    COALESCE(file_format_id, '') AS file_format_id,
+    COALESCE(anatomy_id, '') AS anatomy_id,
+    COALESCE(granularity_id, '') AS granularity_id,
+    COALESCE(species_id, '') AS species_id,
+    count(*) AS num_files,
+    sum(num_bytes) AS num_bytes
+  FROM (
+    SELECT DISTINCT
+      pr.project_id_namespace AS root_project_id_namespace,
+      pr.project_local_id AS root_project_local_id,
+      f.project_id_namespace AS project_id_namespace,
+      f.project_local_id AS project_local_id,
+      f.assay_type assay_type_id,
+      f.data_type AS data_type_id,
+      f.file_format AS file_format_id,
+      b.anatomy AS anatomy_id,
+      s.granularity AS granularity_id,
+      ss.species AS species_id,
+      f."RID" AS files_rid,
+      f.size_in_bytes AS num_bytes
+    FROM file f
+    JOIN project_in_project_transitive pipt
+      ON (    f.project_id_namespace = pipt.member_project_id_namespace
+          AND f.project_locaL_id = pipt.member_project_local_id)
+    JOIN project_root pr
+      ON (    pipt.leader_project_id_namespace = pr.project_id_namespace
+          AND pipt.leader_project_local_id = pr.project_local_id)
+    LEFT JOIN file_describes_biosample fdb
+      ON (    f.id_namespace = fdb.file_id_namespace
+          AND f.local_id = fdb.file_local_id)
+    LEFT JOIN biosample b
+      ON (    fdb.biosample_id_namespace = b.id_namespace
+          AND fdb.biosample_local_id = b.local_id)
+    LEFT JOIN biosample_from_subject bfs
+      ON (    bfs.biosample_id_namespace = b.id_namespace
+          AND bfs.biosample_local_id = b.local_id)
+    LEFT JOIN subject s
+      ON (    bfs.subject_id_namespace = s.id_namespace
+          AND bfs.subject_local_id = s.local_id)
+    LEFT JOIN subject_species ss
+      ON (    s.id_namespace = ss.subject_id_namespace
+          AND s.local_id = ss.subject_local_id)
+    ) s
   GROUP BY
-    pr.project_id_namespace,
-    pr.project_local_id,
-    f.project_id_namespace,
-    f.project_local_id,
-    f.assay_type,
-    f.data_type,
-    f.file_format,
-    b.anatomy,
-    s.granularity,
-    ss.species
+    root_project_id_namespace,
+    root_project_local_id,
+    project_id_namespace,
+    project_local_id,
+    assay_type_id,
+    data_type_id,
+    file_format_id,
+    anatomy_id,
+    granularity_id,
+    species_id
 ) fstats
 FULL OUTER JOIN (
   -- biosample stats need biosample table rows, find optional remote metadata
