ed5599729a50334873c99da923e2b832d62decc3
Karl Czajkowski <karlcz@isi.edu>
2021-07-14 17:11:09 -0700

    wip: add fkeys from core_fact to vocab for singular linkage
    Opportunistically populate a (nullable) fkey for each array-based
    dimension when the cardinality of the array is 1 for a given row.
    
    The core entities will always be able to find their outbound
    relationships via core_fact:
    
    - file -> core_fact -> id_namespace, project, assay_type, data_type, file_format, mime_type
    - biosample -> core_fact -> id_namespace, project, anatomy
    - subject -> id_namespace, project, granularity
    
    This will be helpful for some Chaise functions.

---
+++ cfde_deriva/configs/portal_prep/core_fact_mime_type.sql
@@ -0,0 +1,14 @@
+UPDATE core_fact AS u
+SET mime_type = s.value
+FROM (
+  SELECT
+    s.nid,
+    j.value
+  FROM core_fact s
+  JOIN json_each(s.mime_types) j
+  WHERE j.value IS NOT NULL
+  GROUP BY s.nid, j.value
+  HAVING count(*) = 1
+) s
+WHERE u.nid = s.nid
+;
---
+++ cfde_deriva/configs/portal_prep/core_fact_ncbi_taxon.sql
@@ -0,0 +1,14 @@
+UPDATE core_fact AS u
+SET ncbi_taxon = s.value
+FROM (
+  SELECT
+    s.nid,
+    j.value
+  FROM core_fact s
+  JOIN json_each(s.ncbi_taxons) j
+  WHERE j.value IS NOT NULL
+  GROUP BY s.nid, j.value
+  HAVING count(*) = 1
+) s
+WHERE u.nid = s.nid
+;
