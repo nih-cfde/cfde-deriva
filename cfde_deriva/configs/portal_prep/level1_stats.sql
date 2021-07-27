817c8892e1d294f213795ad372c50ff89e6c5114
Karl Czajkowski <karlcz@isi.edu>
2021-07-27 14:35:40 -0700

    fix data-dependency/execution order bug for level1_stats ETL
    The table-level level1_stats.sql runs before the column-level
    core_fact_num_files.sql etc. and so cannot build results from these
    intermediate aggregates.
    
    Instead, walk fkeys back to actual c2m2 entities and count/sum them
    as needed for each core_fact row.
    
    Also, rewrite using normal SQL joins over the
    core_fact_... associations instead of the SQLite-specific json_each
    table function. This way, we can run the same query to fixup the
    broken level1_stats summary in existing dev catalogs.

--- cfde_deriva/configs/portal_prep/level1_stats.sql
+++ cfde_deriva/configs/portal_prep/level1_stats.sql
@@ -41,26 +41,73 @@ SELECT
   anat.id,
   sg.id,
   ss.id,
-  sum(num_files),
-  sum(total_size_in_bytes),
-  sum(num_biosamples),
-  sum(num_subjects)
+  sum(cff.num_files),
+  sum(cff.size_in_bytes) AS size_in_bytes,
+  sum(cfb.num_biosamples),
+  sum(cfs.num_subjects)
 FROM core_fact cf
-JOIN json_each(cf.projects) proj_j
-JOIN project proj ON (proj_j.value = proj.nid)
-JOIN json_each(cf.dccs) dcc_j
-JOIN dcc ON (dcc_j.value = dcc.nid)
+LEFT JOIN (
+  SELECT
+    cf.nid,
+    count(*) AS num_files,
+    sum(f.size_in_bytes) AS size_in_bytes
+  FROM core_fact cf
+  JOIN file f ON (f.core_fact = cf.nid)
+  GROUP BY cf.nid
+) cff ON (cf.nid = cff.nid)
+LEFT JOIN (
+  SELECT
+    cf.nid,
+    count(*) AS num_biosamples
+  FROM core_fact cf
+  JOIN biosample b ON (b.core_fact = cf.nid)
+  GROUP BY cf.nid
+) cfb ON (cf.nid = cfb.nid)
+LEFT JOIN (
+  SELECT
+    cf.nid,
+    count(*) AS num_subjects
+  FROM core_fact cf
+  JOIN subject s ON (s.core_fact = cf.nid)
+  GROUP BY cf.nid
+) cfs ON (cf.nid = cfs.nid)
+JOIN core_fact_project cf_p ON (cf.nid = cf_p.core_fact)
+JOIN project proj ON (cf_p.project = proj.nid)
+JOIN core_fact_dcc cf_d ON (cf.nid = cf_d.core_fact)
+JOIN dcc ON (cf_d.dcc = dcc.nid)
 JOIN project dcc_proj ON (dcc.project = dcc_proj.nid)
-LEFT JOIN json_each(cf.assay_types) at_j
-LEFT JOIN assay_type "at" ON (at_j.value = "at".nid)
-LEFT JOIN json_each(cf.data_types) dt_j
-LEFT JOIN data_type dt ON (dt_j.value = dt.nid)
-LEFT JOIN json_each(cf.file_formats) ff_j
-LEFT JOIN file_format ff ON (ff_j.value = ff.nid)
-LEFT JOIN json_each(cf.anatomies) anat_j
-LEFT JOIN anatomy anat ON (anat_j.value = anat.nid)
-LEFT JOIN json_each(cf.subject_granularities) sg_j
-LEFT JOIN subject_granularity sg ON (sg_j.value = sg.nid)
-LEFT JOIN json_each(cf.subject_species) ss_j
-LEFT JOIN ncbi_taxonomy ss ON (ss_j.value = ss.nid)
-GROUP BY proj.nid, "at".nid, dt.nid, ff.nid, anat.nid, sg.nid, ss.nid
+LEFT JOIN (
+  core_fact_assay_type cf_at
+  JOIN assay_type "at" ON (cf_at.assay_type = "at".nid)
+) ON (cf.nid = cf_at.core_fact)
+LEFT JOIN (
+  core_fact_data_type cf_dt
+  JOIN data_type dt ON (cf_dt.data_type = dt.nid)
+) ON (cf.nid = cf_dt.core_fact)
+LEFT JOIN (
+  core_fact_file_format cf_ff
+  JOIN file_format ff ON (cf_ff.file_format = ff.nid)
+) ON (cf.nid = cf_ff.core_fact)
+LEFT JOIN (
+  core_fact_anatomy cf_a
+  JOIN anatomy anat ON (cf_a.anatomy = anat.nid)
+) ON (cf.nid = cf_a.core_fact)
+LEFT JOIN (
+  core_fact_subject_granularity cf_sg
+  JOIN subject_granularity sg ON (cf_sg.subject_granularity = sg.nid)
+) ON (cf.nid = cf_sg.core_fact)
+LEFT JOIN (
+  core_fact_subject_species cf_ss
+  JOIN ncbi_taxonomy ss ON (cf_ss.subject_species = ss.nid)
+) ON (cf.nid = cf_ss.core_fact)
+GROUP BY
+  -- group by all extra cols so we can test/run this on postgres too
+  proj.nid, proj.id_namespace, proj.local_id,
+  dcc_proj.nid, dcc_proj.id_namespace, dcc_proj.local_id,
+  "at".nid, "at".id,
+  dt.nid, dt.id,
+  ff.nid, ff.id,
+  anat.nid, anat.id,
+  sg.nid, sg.id,
+  ss.nid, ss.id
+;
