dc7bc54c309a80c0f0cf8d45c83870e3417258c5
Karl Czajkowski <karlcz@isi.edu>
2021-08-04 13:03:41 -0700

    for safety, augment slim map with identity mapping for any alien terms
    If the submission uses terms not considered by the ontology WG, they
    will not be present in the canonical slim map.

--- cfde_deriva/configs/portal_prep/anatomy_slim.sql
+++ cfde_deriva/configs/portal_prep/anatomy_slim.sql
@@ -1,3 +1,4 @@
+-- get slim mappings for every term used
 INSERT INTO anatomy_slim (
   original_term,
   slim_term
@@ -8,4 +9,17 @@ SELECT
 FROM anatomy_slim_raw a
 JOIN anatomy o ON (a.original_term_id = o.id)
 JOIN anatomy s ON (a.slim_term_id = s.id)
+;
+
+-- add identity mapping for any unmapped terms
+INSERT INTO anatomy_slim (
+  original_term,
+  slim_term
+)
+SELECT
+  o.nid,
+  o.nid
+FROM anatomy o
+LEFT JOIN anatomy_slim a ON (o.nid = a.original_term)
+WHERE a.original_term IS NULL
 ;
