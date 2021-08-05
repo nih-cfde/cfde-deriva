faa52350b1eb742087c8750b8584d0256db4576b
Karl Czajkowski <karlcz@isi.edu>
2021-08-05 13:05:01 -0700

    fill in file_describes_subject links implied by file/biosample/subject chains

--- cfde_deriva/configs/portal_prep/file_describes_subject.sql
+++ cfde_deriva/configs/portal_prep/file_describes_subject.sql
@@ -1,3 +1,4 @@
+-- translate submitted links
 INSERT INTO file_describes_subject (
   file,
   subject
@@ -8,4 +9,20 @@ SELECT
 FROM submission.file_describes_subject fds
 JOIN submission.file f ON (fds.file_id_namespace = f.id_namespace AND fds.file_local_id = f.local_id)
 JOIN submission.subject s ON (fds.subject_id_namespace = s.id_namespace AND fds.subject_local_id = s.local_id)
+;
+
+-- add any gaps implied by file -- biosample -- subject
+INSERT INTO file_describes_subject (
+  file,
+  subject
+)
+SELECT
+  f.nid,
+  s.nid
+FROM submission.file_describes_biosample fdb
+JOIN submission.file f ON (fdb.file_id_namespace = f.id_namespace AND fdb.file_local_id = f.local_id)
+JOIN submission.biosample_from_subject bfs USING (biosample_id_namespace, biosample_local_id)
+JOIN submission.subject s ON (bfs.subject_id_namespace = s.id_namespace AND bfs.subject_local_id = s.local_id)
+WHERE True
+ON CONFLICT DO NOTHING
 ;
