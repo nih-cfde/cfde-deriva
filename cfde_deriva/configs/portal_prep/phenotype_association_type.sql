f6dd954d120fd808df1a0ef5b1cb97d671d7963c
Karl Czajkowski <karlcz@isi.edu>
2021-12-01 11:44:49 -0800

    add hack to introduce a nid=0 'not specified' association type
    To integrate collection-level and subject/biosample-level disease
    or phenotype associations, pretend the collection-level ones are marked
    with a special 'not specified' association type, which is introduced
    during ETL, after the DCC submission is validated against the enumerated
    association types (excluding this extra value).

---
+++ cfde_deriva/configs/portal_prep/phenotype_association_type.sql
@@ -0,0 +1,19 @@
+INSERT INTO phenotype_association_type (
+  nid,
+  id,
+  name,
+  description
+)
+SELECT
+  nid,
+  id,
+  name,
+  description
+FROM submission.phenotype_association_type
+UNION
+VALUES (
+  '0',
+  'cfde_disease_association_type:NS',
+  'not specified',
+  'association type not specified'
+);
