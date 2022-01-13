39f4aabd62d528f189d3e564fea6737437f22fe5
Karl Czajkowski <karlcz@isi.edu>
2022-01-13 12:34:35 -0800

    replace core_fact_kw ETL SQL with a version that scales better
    The prior implementation could produce a massive intermediate tuple
    set when run on large core_fact tables.  This became intractable when
    the core_fact row count ballooned with recent submission data.  The
    new implementation scales more reasonably with the number of core_fact
    records.
    
    This version also adds slim term keywords for several additional
    slim maps which were not being incorporated into the index before.

--- cfde_deriva/configs/portal_prep/core_fact_kw.sql
+++ cfde_deriva/configs/portal_prep/core_fact_kw.sql
@@ -17,92 +17,117 @@ FROM (
   SELECT
     cf.nid,
     cfde_keywords_merge(
-      cfde_keywords_agg(n.name, n.abbreviation),
-      cfde_keywords_agg(p.name, p.abbreviation, p.description),
-      cfde_keywords_agg(d.dcc_name, d.dcc_abbreviation, d.dcc_description),
+      cfde_keywords(n.name, n.abbreviation),
 
-      cfde_keywords_agg(pht.id, pht.name, pht.description),
-      cfde_keywords_merge_agg(pht.synonyms),
-      cfde_keywords_agg(dis.id, dis.name, dis.description),
-      cfde_keywords_merge_agg(dis.synonyms),
-      cfde_keywords_agg(subst.id, subst.name, subst.description),
-      cfde_keywords_merge_agg(subst.synonyms),
-      cfde_keywords_agg(cmpd.id, cmpd.name, cmpd.description),
-      cfde_keywords_merge_agg(cmpd.synonyms),
-      cfde_keywords_agg(gn.id, gn.name, gn.description),
-      cfde_keywords_merge_agg(gn.synonyms),
+      (SELECT cfde_keywords_agg(p.name, p.abbreviation, p.description)
+       FROM json_each(cf.projects) pj JOIN project p ON (pj.value = p.nid)),
 
-      cfde_keywords_agg(sx.id, sx.name, sx.description),
-      cfde_keywords_agg(rc.id, rc.name, rc.description),
-      cfde_keywords_agg(eth.id, eth.name, eth.description),
-      cfde_keywords_agg(sr.name, sr.description),
-      cfde_keywords_agg(sg.name, sg.description),
-      cfde_keywords_agg(t.id, t.name, t.description),
-      cfde_keywords_merge_agg(t.synonyms),
+      (SELECT cfde_keywords_agg(d.dcc_name, d.dcc_abbreviation, d.dcc_description)
+       FROM json_each(cf.dccs) dj JOIN dcc d ON (dj.value = d.nid)),
 
-      cfde_keywords_agg(a.id, a.name, a.description),
-      cfde_keywords_merge_agg(a.synonyms),
-      cfde_keywords_agg(a2.id, a2.name, a2.description),
-      cfde_keywords_merge_agg(a2.synonyms),
+      (SELECT cfde_keywords_merge(
+         cfde_keywords_agg(pht.id, pht.name, pht.description),
+         cfde_keywords_merge_agg(pht.synonyms)
+       )
+       FROM json_each(cf.phenotypes) phtj JOIN phenotype pht ON (json_extract(phtj.value, '$[0]') = pht.nid)),
 
-      cfde_keywords_agg("at".id, "at".name, "at".description),
-      cfde_keywords_merge_agg("at".synonyms),
-      cfde_keywords_agg(ant.id, ant.name, ant.description),
-      cfde_keywords_merge_agg(ant.synonyms),
-      cfde_keywords_agg(ff.id, ff.name, ff.description),
-      cfde_keywords_merge_agg(ff.synonyms),
-      cfde_keywords_agg(dt.id, dt.name, dt.description),
-      cfde_keywords_merge_agg(dt.synonyms),
-      cfde_keywords_agg(mt.id, mt.name, mt.description)
+      (SELECT cfde_keywords_merge(
+         cfde_keywords_agg(dis.id, dis.name, dis.description),
+         cfde_keywords_merge_agg(dis.synonyms),
+         cfde_keywords_agg(dis2.id, dis2.name, dis2.description),
+         cfde_keywords_merge_agg(dis2.synonyms)
+       )
+       FROM json_each(cf.diseases) disj JOIN disease dis ON (json_extract(disj.value, '$[0]') = dis.nid)
+       JOIN disease_slim dis_slim ON (disj.value = dis_slim.original_term) JOIN disease dis2 ON (dis_slim.slim_term = dis.nid)),
+
+      (SELECT cfde_keywords_merge(
+         cfde_keywords_agg(subst.id, subst.name, subst.description),
+         cfde_keywords_merge_agg(subst.synonyms)
+       )
+       FROM json_each(cf.substances) substj JOIN substance subst ON (substj.value = subst.nid)),
+
+      (SELECT cfde_keywords_merge(
+        cfde_keywords_agg(cmpd.id, cmpd.name, cmpd.description),
+        cfde_keywords_merge_agg(cmpd.synonyms)
+       )
+       FROM json_each(cf.compounds) cmpdj JOIN compound cmpd ON (cmpdj.value = cmpd.nid)),
+
+      (SELECT cfde_keywords_merge(
+         cfde_keywords_agg(gn.id, gn.name, gn.description),
+         cfde_keywords_merge_agg(gn.synonyms)
+       )
+       FROM json_each(cf.genes) gnj JOIN gene gn ON (gnj.value = gn.nid)),
+
+      (SELECT cfde_keywords_agg(sx.id, sx.name, sx.description)
+       FROM json_each(cf.sexes) sxj JOIN sex sx ON (sxj.value = sx.nid)),
+
+      (SELECT cfde_keywords_agg(rc.id, rc.name, rc.description)
+       FROM json_each(cf.races) rcj JOIN race rc ON (rcj.value = rc.nid)),
+
+      (SELECT cfde_keywords_agg(eth.id, eth.name, eth.description)
+       FROM json_each(cf.ethnicities) ethj JOIN ethnicity eth ON (ethj.value = eth.nid)),
+
+      (SELECT cfde_keywords_agg(sr.name, sr.description)
+       FROM json_each(cf.subject_roles) srj JOIN subject_role sr ON (srj.value = sr.nid)),
+
+      (SELECT cfde_keywords_agg(sg.name, sg.description)
+       FROM json_each(cf.subject_granularities) sgj JOIN subject_granularity sg ON (sgj.value = sg.nid)),
+
+      (SELECT cfde_keywords_merge(
+         cfde_keywords_agg(t.id, t.name, t.description),
+         cfde_keywords_merge_agg(t.synonyms)
+       )
+       FROM json_each(cf.ncbi_taxons) tj JOIN ncbi_taxonomy t ON (tj.value = t.nid)),
+
+      (SELECT cfde_keywords_merge(
+         cfde_keywords_agg(a.id, a.name, a.description),
+         cfde_keywords_merge_agg(a.synonyms),
+         cfde_keywords_agg(a2.id, a2.name, a2.description),
+         cfde_keywords_merge_agg(a2.synonyms)
+       )
+       FROM json_each(cf.anatomies) aj JOIN anatomy a ON (aj.value = a.nid)
+       JOIN anatomy_slim a_slim ON (aj.value = a_slim.original_term) JOIN anatomy a2 ON (a_slim.slim_term = a2.nid)),
+
+      (SELECT cfde_keywords_merge(
+         cfde_keywords_agg("at".id, "at".name, "at".description),
+         cfde_keywords_merge_agg("at".synonyms),
+         cfde_keywords_agg("at2".id, "at2".name, "at2".description),
+         cfde_keywords_merge_agg("at2".synonyms)
+       )
+       FROM json_each(cf.assay_types) atj JOIN assay_type "at" ON (atj.value = "at".nid)
+       JOIN assay_type_slim at_slim ON (atj.value = at_slim.original_term) JOIN assay_type at2 ON (at_slim.slim_term = at2.nid)),
+
+      (SELECT cfde_keywords_merge(
+         cfde_keywords_agg(ant.id, ant.name, ant.description),
+         cfde_keywords_merge_agg(ant.synonyms)
+       )
+       FROM json_each(cf.analysis_types) antj JOIN analysis_type ant ON (antj.value = ant.nid)),
+
+      (SELECT cfde_keywords_merge(
+         cfde_keywords_agg(ff.id, ff.name, ff.description),
+         cfde_keywords_merge_agg(ff.synonyms),
+         cfde_keywords_agg(ff2.id, ff2.name, ff2.description),
+         cfde_keywords_merge_agg(ff2.synonyms)
+       )
+       FROM json_each(cf.file_formats) ffj JOIN file_format ff ON (ffj.value = ff.nid)
+       JOIN file_format_slim ff_slim ON (ffj.value = ff_slim.original_term) JOIN file_format ff2 ON (ff_slim.slim_term = ff2.nid)),
+
+      (SELECT cfde_keywords_merge(
+         cfde_keywords_agg(dt.id, dt.name, dt.description),
+         cfde_keywords_merge_agg(dt.synonyms),
+         cfde_keywords_agg(dt2.id, dt2.name, dt2.description),
+         cfde_keywords_merge_agg(dt2.synonyms)
+       )
+       FROM json_each(cf.data_types) dtj JOIN data_type dt ON (dtj.value = dt.nid)
+       JOIN data_type_slim dt_slim ON (dtj.value = dt_slim.original_term) JOIN data_type dt2 ON (dt_slim.slim_term = dt2.nid)),
+
+      (SELECT cfde_keywords_agg(mt.id, mt.name, mt.description)
+       FROM json_each(cf.mime_types) mtj JOIN mime_type mt ON (mtj.value = mt.nid))
+
     ) AS kw
   FROM core_fact cf
   JOIN id_namespace n ON (cf.id_namespace = n.nid)
-  LEFT JOIN json_each(cf.projects) pj
-  LEFT JOIN project p ON (pj.value = p.nid)
-  LEFT JOIN json_each(cf.dccs) dj
-  LEFT JOIN dcc d ON (dj.value = d.nid)
 
-  LEFT JOIN json_each(cf.phenotypes) phtj
-  LEFT JOIN phenotype pht ON (json_extract(phtj.value, '$[0]') = pht.nid)
-  LEFT JOIN json_each(cf.diseases) disj
-  LEFT JOIN disease dis ON (json_extract(disj.value, '$[0]') = dis.nid)
-  LEFT JOIN json_each(cf.substances) substj
-  LEFT JOIN substance subst ON (substj.value = subst.nid)
-  LEFT JOIN json_each(cf.compounds) cmpdj
-  LEFT JOIN compound cmpd ON (cmpdj.value = cmpd.nid)
-  LEFT JOIN json_each(cf.genes) gnj
-  LEFT JOIN gene gn ON (gnj.value = gn.nid)
-
-  LEFT JOIN json_each(cf.sexes) sxj
-  LEFT JOIN sex sx ON (sxj.value = sx.nid)
-  LEFT JOIN json_each(cf.races) rcj
-  LEFT JOIN race rc ON (rcj.value = rc.nid)
-  LEFT JOIN json_each(cf.ethnicities) ethj
-  LEFT JOIN ethnicity eth ON (ethj.value = eth.nid)
-  LEFT JOIN json_each(cf.subject_roles) srj
-  LEFT JOIN subject_role sr ON (srj.value = sr.nid)
-  LEFT JOIN json_each(cf.subject_granularities) sgj
-  LEFT JOIN subject_granularity sg ON (sgj.value = sg.nid)
-  LEFT JOIN json_each(cf.ncbi_taxons) tj
-  LEFT JOIN ncbi_taxonomy t ON (tj.value = t.nid)
-
-  LEFT JOIN json_each(cf.anatomies) aj
-  LEFT JOIN anatomy a ON (aj.value = a.nid)
-  LEFT JOIN anatomy_slim a_slim ON (aj.value = a_slim.original_term)
-  LEFT JOIN anatomy a2 ON (a_slim.slim_term = a2.nid)
-
-  LEFT JOIN json_each(cf.assay_types) atj
-  LEFT JOIN assay_type "at" ON (atj.value = "at".nid)
-  LEFT JOIN json_each(cf.analysis_types) antj
-  LEFT JOIN analysis_type ant ON (antj.value = ant.nid)
-  LEFT JOIN json_each(cf.file_formats) ffj
-  LEFT JOIN file_format ff ON (ffj.value = ff.nid)
-  LEFT JOIN json_each(cf.data_types) dtj
-  LEFT JOIN data_type dt ON (dtj.value = dt.nid)
-  LEFT JOIN json_each(cf.mime_types) mtj
-  LEFT JOIN mime_type mt ON (mtj.value = mt.nid)
-
-  GROUP BY cf.nid
 ) s
 WHERE v.nid = s.nid
 ;
