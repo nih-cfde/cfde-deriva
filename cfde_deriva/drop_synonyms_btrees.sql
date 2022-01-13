171be1fa58fe7742762fd10ec755354e99473f30
Karl Czajkowski <karlcz@isi.edu>
2022-01-13 14:36:57 -0800

    add drop_synonyms_btrees.sql mentioned in earlier commit

---
+++ cfde_deriva/drop_synonyms_btrees.sql
@@ -0,0 +1,12 @@
+
+DROP INDEX IF EXISTS "CFDE".analysis_type_synonyms_idx;
+DROP INDEX "CFDE".anatomy_synonyms_idx;
+DROP INDEX "CFDE".assay_type_synonyms_idx;
+DROP INDEX "CFDE".compound_synonyms_idx;
+DROP INDEX "CFDE".data_type_synonyms_idx;
+DROP INDEX "CFDE".disease_synonyms_idx;
+DROP INDEX "CFDE".file_format_synonyms_idx;
+DROP INDEX "CFDE".gene_synonyms_idx;
+DROP INDEX "CFDE".ncbi_taxonomy_synonyms_idx;
+DROP INDEX IF EXISTS "CFDE".phenotype_synonyms_idx;
+DROP INDEX "CFDE".substance_synonyms_idx;
