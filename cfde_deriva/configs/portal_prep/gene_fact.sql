a2208faeb1013635a2a4a161cd15f6ac5e737891
Karl Czajkowski <karlcz@isi.edu>
2022-03-09 13:46:49 -0800

    shift gene/protein/pubchem fact table kw ETL to earlier table ETL pass
    Generate these earlier in the sequence so they can be consumed by later
    table and column ETL queries.

--- cfde_deriva/configs/portal_prep/gene_fact.sql
+++ cfde_deriva/configs/portal_prep/gene_fact.sql
@@ -122,3 +122,20 @@ FROM collection_gfacts colf, gene_fact gf
 WHERE u.nid = colf.nid
   AND colf.genes = gf.genes
 ;
+
+UPDATE gene_fact AS v
+SET kw = s.kw
+FROM (
+  SELECT
+    gf.nid,
+    cfde_keywords_merge(
+      (SELECT cfde_keywords_merge(
+         cfde_keywords_agg(gn.id, gn.name, gn.description),
+         cfde_keywords_merge_agg(gn.synonyms)
+       )
+       FROM json_each(gf.genes) gnj JOIN gene gn ON (gnj.value = gn.nid))
+    ) AS kw
+  FROM gene_fact gf
+) s
+WHERE v.nid = s.nid
+;
--- cfde_deriva/configs/portal_prep/protein_fact.sql
+++ cfde_deriva/configs/portal_prep/protein_fact.sql
@@ -47,3 +47,20 @@ FROM collection_prfacts colf, protein_fact prf
 WHERE u.nid = colf.nid
   AND colf.proteins = prf.proteins
 ;
+
+UPDATE protein_fact AS v
+SET kw = s.kw
+FROM (
+  SELECT
+    prf.nid,
+    cfde_keywords_merge(
+      (SELECT cfde_keywords_merge(
+         cfde_keywords_agg(pr.id, pr.name, pr.description),
+         cfde_keywords_merge_agg(pr.synonyms)
+       )
+       FROM json_each(prf.proteins) prj JOIN protein pr ON (prj.value = pr.nid))
+    ) AS kw
+  FROM protein_fact prf
+) s
+WHERE v.nid = s.nid
+;
