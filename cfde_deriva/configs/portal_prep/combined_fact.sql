4c7c46e896792652b9b8a78008d9efe700de1850
Karl Czajkowski <karlcz@isi.edu>
2022-03-08 18:00:13 -0800

    wip: denormalize combined_fact.kw and link combined_fact to core c2m2 entities

--- cfde_deriva/configs/portal_prep/combined_fact.sql
+++ cfde_deriva/configs/portal_prep/combined_fact.sql
@@ -15,6 +15,15 @@ FROM collection
 GROUP BY core_fact, gene_fact, protein_fact, pubchem_fact
 ;
 
+UPDATE collection AS v
+SET combined_fact = s.nid
+FROM combined_fact s
+WHERE v.core_fact = s.core_fact
+  AND v.gene_fact = s.gene_fact
+  AND v.pubchem_fact = s.pubchem_fact
+  AND v.protein_fact = s.protein_fact
+;
+
 INSERT INTO combined_fact (
   core_fact,
   gene_fact,
@@ -39,6 +48,15 @@ SET num_files = EXCLUDED.num_files,
     total_size_in_bytes = EXCLUDED.total_size_in_bytes
 ;
 
+UPDATE file AS v
+SET combined_fact = s.nid
+FROM combined_fact s
+WHERE v.core_fact = s.core_fact
+  AND v.gene_fact = s.gene_fact
+  AND v.pubchem_fact = s.pubchem_fact
+  AND v.protein_fact = s.protein_fact
+;
+
 INSERT INTO combined_fact (
   core_fact,
   gene_fact,
@@ -60,6 +78,15 @@ DO UPDATE
 SET num_biosamples = EXCLUDED.num_biosamples
 ;
 
+UPDATE biosample AS v
+SET combined_fact = s.nid
+FROM combined_fact s
+WHERE v.core_fact = s.core_fact
+  AND v.gene_fact = s.gene_fact
+  AND v.pubchem_fact = s.pubchem_fact
+  AND v.protein_fact = s.protein_fact
+;
+
 INSERT INTO combined_fact (
   core_fact,
   gene_fact,
@@ -79,4 +106,13 @@ GROUP BY core_fact, gene_fact, protein_fact, pubchem_fact
 ON CONFLICT (core_fact, gene_fact, protein_fact, pubchem_fact)
 DO UPDATE
 SET num_subjects = EXCLUDED.num_subjects
+;
+
+UPDATE subject AS v
+SET combined_fact = s.nid
+FROM combined_fact s
+WHERE v.core_fact = s.core_fact
+  AND v.gene_fact = s.gene_fact
+  AND v.pubchem_fact = s.pubchem_fact
+  AND v.protein_fact = s.protein_fact
 ;
